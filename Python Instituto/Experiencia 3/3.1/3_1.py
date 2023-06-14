0150017d69f9c0233ce21606c072bde3367442bf
Maximiliano Hernández <hernandez.gallardo81@gmail.com>
2023-06-14 18:22:15 -0400

    Se realiza todo lo de la experiencia 3.1

---
+++ Python Instituto/Experiencia 3/3.1/3_1.py
@@ -0,0 +1,41 @@
+#                               -- Listas --
+
+
+
+# la imagen muestra que en cada caja podemos guardar un valor, sea este una variable númerica, cadenas de caracteres, incluso una lista.abs
+
+# lista = [1, 'Hola', 3.67, [1, 2, 3]]
+
+# Se asemejan bastante a los Arrays, so consideramos otros lenguajes de programacion.
+# Si necesitamos incluir más de un valor dentro de la lista, sólo los separamos por una coma.abs
+
+# listas = [1, 2, 3, 4]
+
+# -- Crear Listas --
+
+# nombre_lista = []
+
+# nombre_lista --> NOMBRE DE LA LISTA
+# [] --> ESTOS PARÉNTESIS DE CORCHETE INDICAN QUE ES UNA LISTA
+
+
+
+
+
+#                               -- Elementos de una lista --
+
+
+
+# El ejemplo muestra que posee 3 elementos y que para acceder a ellos, solo se utiliza un índice
+
+# a = [12, 'Python', 3.1416]
+
+# print(a[0]) #[12]       -->  El elemento 1 está en la posición 0
+# print(a[1]) #[Python]   -->  El elemento 2 esta en la posicion 1
+# print(a[2]) #[3.1416]   -->  El elemento 3 esta en la posicion 2
+
+# Para acceder al último elemento de la lista, sólo se utiliza el índice -1
+
+a = [90, 'Python', 3.87]
+
+print(a[-2])
