a30081a9433f92b01a424f25198158fd52cf572f
maximilianoahg <115019471+maximilianoahg@users.noreply.github.com>
2023-06-14 14:35:53 +0000

    Se realiza el Aprendizaje 3.1

---
+++ Python Instituto/Experiencia de Aprendizaje 3: Programando Listas y Funciones/3.1/3_1.py
@@ -0,0 +1,27 @@
+# -- Listas --
+
+# la imagen muestra que en cada caja podemos guardar un valor, sea este una variable númerica, cadenas de caracteres, incluso una lista.abs
+
+# lista = [1, 'Hola', 3.67, [1, 2, 3]]
+
+# Se asemejan bastante a los Arrays, so consideramos otros lenguajes de programacion.
+# Si necesitamos incluir más de un valor dentro de la lista, sólo los separamos por una coma.abs
+
+# listas = [1, 2, 3, 4]
+
+# -- Crear Listas --
+
+# nombre_lista = []
+
+# nombre_lista --> NOMBRE DE LA LISTA
+# [] --> ESTOS PARÉNTESIS DE CORCHETE INDICAN QUE ES UNA LISTA
+
+# -- Elementos de una lista --
+
+# El ejemplo muestra que posee 3 elementos y que para acceder a ellos, solo se utiliza un índice
+
+a = [12, 'Python', 3.1416]
+
+print(a[0])
+print(a[1])
+print(a[2])
